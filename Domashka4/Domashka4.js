8e33cfbe15a2a6ade6e70c1ef8979d4887995064
8Artem800 <116750353+8Artem800@users.noreply.github.com>
2022-12-07 18:34:33 +0300

    Add files via upload

---
+++ Domashka4/Domashka4.js
@@ -0,0 +1,48 @@
+
+let arr =["1,2,3"]
+alert(arr);
+arr.unshift("5,8,9");
+alert(arr);
+
+
+
+alert("задание №3");
+let arr3 = ['a', 'b', 'c'];
+arr3.push('1, 2 , 3');
+alert(arr3);
+
+
+
+alert("задание №4");
+let arr4 = ['js', 'css', 'jq'];
+alert(arr4);
+alert(arr4.pop());
+alert(arr4);
+
+
+
+alert("задание №5");
+let arr5 = ['js', 'css', 'jq'];
+alert(arr5);
+alert(arr5.shift());
+alert(arr5);
+
+
+
+alert("задание №6");
+let arr6 =["1,2,3"];
+alert(arr6);
+arr6.unshift("4, 5, 6");
+alert(arr6);
+
+
+let arr8 = ['Джаз', 'Блюз'];
+alert(arr8);
+arr8.push('Рок-н-ролл');
+alert(arr8);
+arr8[1] = 'Классика';
+alert(arr8);
+alert(arr8.shift());
+alert(arr8);
+arr8.unshift('Рэп', 'Регги');
+alert(arr8);
